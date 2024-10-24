6853f394cecca63e251526116d6cce60c3e59813
JuanHuisi <jcromero@dc.uba.ar>
2024-10-24 16:02:59 -0300

    chequeo de bipartición

--- igna, martin y la bipartitud/agm.cpp
+++ igna, martin y la bipartitud/agm.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <set>
+
 
 using namespace std;
 
-void dfs(vector<vector<int>>& grafo,vector<int>& padres, int nodo_actual, vector<bool>& visitados){
+/* void dfs(vector<vector<int>>& grafo,vector<int>& padres, int nodo_actual, vector<bool>& visitados){
     
     
     visitados[nodo_actual] = true;
@@ -17,8 +18,47 @@ void dfs(vector<vector<int>>& grafo,vector<int>& padres, int nodo_actual, vector
         }
     }
     
+} */
+
+
+bool dfs(vector<vector<int>>& grafo, int nodo_actual, vector<int>& grupo, int grupo_actual){
+    
+    grupo[nodo_actual] = grupo_actual;
+    
+
+    for(int vecino : grafo[nodo_actual]){
+        if(grupo[vecino] == -1){
+            if(dfs(grafo, vecino, grupo, 1-grupo_actual)){
+                return false;
+            }
+        }else if(grupo[vecino] == grupo_actual){
+            return false;
+        }
+    }
+    
 }
 
+
+bool esBipartito(vector<vector<int>>& grafo,int vertices,set<int>& conj_A,set<int>& conj_B){
+    // para ver si el grafo es bipartito necesito guardar los nodos en un dos conjuntos 
+    vector<int> conjunto(vertices, -1);
+
+    for(int i = 0; i < vertices; i++){
+        if(conjunto[i] == -1){
+            if(!dfs(grafo,0,conjunto,0)){
+                return false;
+            }
+        }
+    }
+    for(int i = 0; i < vertices; i++){
+        if(conjunto[i] == 0)
+        {conj_A.insert(i);
+        }else{conj_B.insert(i);}
+    }
+
+    return true;
+
+}
 
 
 int main() {
@@ -38,11 +78,14 @@ int main() {
 
     vector<bool> visitados(vertices, false);
     vector<int> padres(vertices, -1);
-    dfs(lista_adyacencia, padres, nodo, visitados);
+    bool res = esBipartito(lista_adyacencia, nodo);
 
-    for(int i : padres){
-        cout << i << " ";
-    }
+    
+    //Una vez evaluado si el arbol es bipartito, podemos simplemente calcular
+    //cuantas aristas le restan para llenar un grafo bipartito.
+    // |A|x|B| - |A|
+
+
 
 
     return 0;
